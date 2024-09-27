a42908430b31a248ff0abc45600deb9320c49a41
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-09-27 08:47:00 -0300

    feat: add svg instagram e linkedin

--- src/components/Icon/svgs/_index.tsx
+++ src/components/Icon/svgs/_index.tsx
@@ -1,2 +1,4 @@
 export { default as default_icon } from './default_icon'
 export { default as github } from './github'
+export { default as instagram } from './instagram'
+export { default as linkedin } from './linkedin'
